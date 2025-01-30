f1c0aca62a290402e1ba5fd59a99827cda00a03b
Riya102293 <riya.verma@coupa.com>
2025-01-30 14:12:57 +0530

    Added Articles Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
