0dbc192f787ef16a1620713731a62b9bfc20f81b
Riya102293 <riya.verma@coupa.com>
2025-02-04 15:02:41 +0530

    CRUD Operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
