eac3865e4ae51e3a65f19cb71b65488406c6d717
Riya102293 <riya.verma@coupa.com>
2025-02-10 14:27:52 +0530

    Restrict actions & delete user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   
   get "up" => "rails/health#show", as: :rails_health_check
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_05_072904) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_10_074027) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_05_072904) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
